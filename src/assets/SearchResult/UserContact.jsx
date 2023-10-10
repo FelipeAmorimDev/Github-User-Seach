228b16dc84925d1859232c70abdcf5b53828b462
Felipe Amorim <felipeamorim.go@gmail.com>
2023-10-10 00:15:42 -0300

    Adicionado opacity nos dados inexistentes

--- src/assets/SearchResult/UserContact.jsx
+++ src/assets/SearchResult/UserContact.jsx
@@ -5,19 +5,19 @@ const UserContact = ({ theme, data }) => {
     <ul
       className="usercontact__list"
       style={{ color: theme === "dark" ? "#fff" : "#4B6A9B" }}>
-      <li className="usercontact__item">
+      <li className="usercontact__item" style={{opacity: data.location === null ? ".5" : "1"}}>
         <img src="./assets/icon-location.svg" alt="" />
         <span>{data.location !== null ? data.location : "Not available"}</span>
       </li>
-      <li className="usercontact__item">
+      <li className="usercontact__item" style={{opacity: data.twitter_username === null ? ".5" : "1"}}>
         <img src="./assets/icon-twitter.svg" alt="" />
         <span>{data.twitter_username !== null ? data.twitter_username : "Not available"}</span>
       </li>
-      <li className="usercontact__item">
+      <li className="usercontact__item" style={{opacity: data.blog === "" ? ".5" : "1"}}>
         <img src="./assets/icon-website.svg" alt="" />
         <span>{data.blog !== "" ? data.blog : "Not available"}</span>
       </li>
-      <li className="usercontact__item">
+      <li className="usercontact__item" style={{opacity: data.company=== null ? ".5" : "1"}}>
         <img src="./assets/icon-company.svg" alt="" />
         <span>{data.company !== null ? data.company : "Not available"}</span>
       </li>
