38c87e6c3a8ee033a4b3db8d2f589bafac6977d5
Felipe Amorim <felipeamorim.go@gmail.com>
2023-10-11 05:50:35 -0300

    Refatoração: SearchResultHeader

--- src/components/SearchResult/SearchResultHeader.jsx
+++ src/components/SearchResult/SearchResultHeader.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 
-
 const SearchResultHeader = ({ theme, data }) => {
+  const titleStyle = { color: theme === "dark" ? "#fff" : "#2B3442" }
+  const joinDateStyle = { color: theme === "dark" ? "#fff" : "#697C9A" }
+  const bioStyle = { color: theme === "dark" ? "#fff" : "#4B6A9B" }
+
   return (
     <>
-      <img src={data.avatar_url} alt="" className='searchresult__userlogo' />
+      <img src={data.avatar_url} alt="Avatar Usuario" className='searchresult__userlogo' />
       <header className='searchresult__top'>
         <div>
           <h1
             className='searchresult__nickname'
-            style={{ color: theme === "dark" ? "#fff" : "#2B3442" }}
+            style={titleStyle}
           >{data.name}</h1>
           <span className='searchresult__id'>@{data.login}</span>
         </div>
         <span
           className='searchresult__joindate'
-          style={{ color: theme === "dark" ? "#fff" : "#697C9A" }}>
+          style={joinDateStyle}>
           Joined 25 Jan 2011
         </span>
       </header>
       <p
         className='searchresult__description'
-        style={{ color: theme === "dark" ? "#fff" : "#4B6A9B" }}
+        style={bioStyle}
       >{data.bio}</p>
     </>
   )
